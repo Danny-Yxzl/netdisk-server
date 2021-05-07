bd975e1723c7be49305d7fefca54f8cda2e53ec3
@Yxiangzhilv <danny070601@aliyun.com>
2021-05-07 23:44:00 +0800

    Update secret_tools.py

--- root/secret_tools.py
+++ root/secret_tools.py
@@ -1,7 +1,7 @@
 # 这是私有工具包，存放加密算法等敏感信息
 
 
-from textChecker import check as check_text  # 违规字符串检测
+# from textChecker import check as check_text  # 违规字符串检测, not necessary.
 
 
 # 必须包括内容：加密和解密字符串算法（两者必须可互逆运算）
